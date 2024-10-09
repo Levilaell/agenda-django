94c20466df9e9526230ccd10462b235c2dac6d69
Levilaell <levilael2@hotmail.com>
2024-10-09 14:24:37 -0300

    add show and picture fields to contact model

--- contact/admin.py
+++ contact/admin.py
@@ -4,4 +4,4 @@ from contact import models
 # Register your models here.
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id', 'first_name', 'last_name', 'phone'
